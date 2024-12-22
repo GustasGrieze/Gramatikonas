c05aec51fc00bb2c60ca2e54a8c4e8c822c7b144
GustasGrieze <106992606+GustasGrieze@users.noreply.github.com>
2024-12-22 20:23:35 +0200

    Add tests

---
+++ TestProject/Models/SpellingTaskTests.cs
@@ -0,0 +1,25 @@
+﻿using lithuanian_language_learning_tool.Models;
+using Xunit;
+
+namespace TestProject.Models
+{
+    public class SpellingTaskTests
+    {
+        [Theory]
+        [InlineData(true, 1, 15)]  // Correct answer, multiplier 1
+        [InlineData(true, 2, 30)]  // Correct answer, multiplier 2
+        [InlineData(false, 1, 0)]  // Incorrect answer, multiplier 1
+        [InlineData(false, 3, 0)]  // Incorrect answer, multiplier 3
+        public void CalculateScore_ShouldReturnCorrectValues(bool isCorrect, int multiplier, int expectedScore)
+        {
+            // Arrange
+            var task = new SpellingTask();
+
+            // Act
+            var score = task.CalculateScore(isCorrect, multiplier);
+
+            // Assert
+            Assert.Equal(expectedScore, score);
+        }
+    }
+}
